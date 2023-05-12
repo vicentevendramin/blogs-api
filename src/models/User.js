62597f369ef16787930c536ec20dfbfa58479f83
Vicente Vendramin <guesser.vicente@gmail.com>
2023-05-12 11:48:50 -0300

    feat: requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      displayName: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false, // remove createdAt and updatedAt
+      underscored: true,
+      tableName: 'users',
+    }
+  );
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'user_id',
+      as: 'blogPosts',
+    });
+  }
+  return User;
+};
